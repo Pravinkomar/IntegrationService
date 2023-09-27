9f4ab42ccd6ed2cf1d79b8f34d824f98ea7863a0
Pravinkomar <139016631+Pravinkomar@users.noreply.github.com>
2023-09-27 15:06:00 +0200

    Create MessageTrigger.cs

---
+++ Models/MessageTrigger.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlDependancyTest.Models
+{
+    public class MessageTrigger
+    {
+        public Guid Id { get; set; }
+        public int MessageStatusId { get; set; }
+    }
+}

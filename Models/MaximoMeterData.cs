fd187cd8fae636bd353689b8e7e63c791259ea6c
Pravinkomar <139016631+Pravinkomar@users.noreply.github.com>
2023-09-27 15:05:31 +0200

    Create MaximoMeterData.cs

---
+++ Models/MaximoMeterData.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegrationService.Models
+{
+    class MaximoMeterData
+    {
+        public int MeterRow;
+        public string MeterName;
+        public string UnitsToGo;
+        public string Frequency;
+
+    }
+}

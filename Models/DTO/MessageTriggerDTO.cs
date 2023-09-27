67a7550d01e9d6cd74143baa151dd70bc3685272
Pravinkomar <139016631+Pravinkomar@users.noreply.github.com>
2023-09-27 15:01:21 +0200

    Create MessageTriggerDTO.cs

---
+++ Models/DTO/MessageTriggerDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlDependancyTest.Models.DTO
+{
+    public class MessageTriggerDTO
+    {
+        public Guid Id { get; set; }
+        public int MessageStatusId { get; set; }
+    }
+}

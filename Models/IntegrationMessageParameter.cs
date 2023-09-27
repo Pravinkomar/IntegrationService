7f4a6ce6b7af0cfe6388b0e51644741f454d7514
Pravinkomar <139016631+Pravinkomar@users.noreply.github.com>
2023-09-27 15:03:29 +0200

    Create IntegrationMessageParameter.cs

---
+++ Models/IntegrationMessageParameter.cs
@@ -0,0 +1,12 @@
+sing System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlDependancyTest.Models
+{
+    public class IntegrationMessageParameter
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}

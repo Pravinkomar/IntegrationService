7b0362875cdfec2360183b4bae245f3e2d799e0f
Pravinkomar <139016631+Pravinkomar@users.noreply.github.com>
2023-09-27 15:00:50 +0200

    Create IntegrationMessageParameterDTO.cs

---
+++ Models/DTO/IntegrationMessageParameterDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlDependancyTest.Models.DTO
+{
+    class IntegrationMessageParameterDTO
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}

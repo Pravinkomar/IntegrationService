6330d93a1dc666099d06caa08efd7d1c9e951760
Pravinkomar <139016631+Pravinkomar@users.noreply.github.com>
2023-09-28 13:20:57 +0200

    Update spCleanMessageTables.sql

--- dbo/Integration/Stored Procedures/spCleanMessageTables.sql
+++ dbo/Integration/Stored Procedures/spCleanMessageTables.sql
@@ -1,1 +1,17 @@
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [Integration].[spGetItemTransfer]
+	@messageId uniqueidentifier
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
 
+    select ParameterName as Name, ParameterValue as Value
+	from Integration.MessageOutParameters  where MessageOutId=@messageId
+
+END

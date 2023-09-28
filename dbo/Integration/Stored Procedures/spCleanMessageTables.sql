6a8f26ed5dde9ee872a8f29eb419d583798e9d45
Pravinkomar <139016631+Pravinkomar@users.noreply.github.com>
2023-09-28 13:22:40 +0200

    Update spCleanMessageTables.sql

--- dbo/Integration/Stored Procedures/spCleanMessageTables.sql
+++ dbo/Integration/Stored Procedures/spCleanMessageTables.sql
@@ -1,17 +1,27 @@
+
 -- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
+-- Author:		EB
+-- Create date: 20180904
+-- Description:	Cleans the MessageTables.
 -- =============================================
-CREATE PROCEDURE [Integration].[spGetItemTransfer]
-	@messageId uniqueidentifier
+CREATE PROCEDURE Integration.spCleanMessageTables
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-    select ParameterName as Name, ParameterValue as Value
-	from Integration.MessageOutParameters  where MessageOutId=@messageId
+    -- Delete from parameters
+	DELETE FROM Integration.MessageOutParameters where messageoutId in (
+		SELECT Id from MessageOut where LastUpdated < DATEADD(MONTH, -1, GETDATE()) 
+		and MessageStatusId = (SELECT TOP 1 Id from Integration.MessageStatuses where description = 'Message Sent (OK)'))
+	-- Delete from MessageOut
+	DELETE FROM Integration.MessageOut where LastUpdated < DATEADD(MONTH, -1, GETDATE()) 
+		and MessageStatusId = (SELECT TOP 1 Id from Integration.MessageStatuses where description = 'Message Sent (OK)')
+	-- Delete from MessageOutHistory
+	DELETE FROM Integration.MessageOutHistory where LastUpdated < DATEADD(MONTH, -7, GETDATE()) 
+		and MessageStatusId = (SELECT TOP 1 Id from Integration.MessageStatuses where description = 'Message Sent (OK)')
 
+	DELETE FROM Integration.MessageIn where LastUpdated < DATEADD(MONTH, -1, GETDATE()) 
+		and MessageStatusId = (SELECT TOP 1 Id from Integration.MessageStatuses where description = 'Message Sent (OK)')
 END
